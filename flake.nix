76db92482eb6001c3f12dbf8f93427c6d1774960
Daniel Baker <daniel.baker@tweag.io>
2023-04-20 08:00:12 -0700

    working state

--- flake.nix
+++ flake.nix
@@ -4,24 +4,51 @@
   inputs.flake-compat.url = "github:edolstra/flake-compat";
   inputs.flake-compat.flake = false;
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.npmlock2nix.url = "github:nix-community/npmlock2nix";
+  inputs.npmlock2nix.flake = false;
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-compat,
-    flake-utils,
-  } @ inputs:
+  outputs =
+    { self
+    , nixpkgs
+    , flake-compat
+    , flake-utils
+    , npmlock2nix
+    ,
+    } @ inputs:
     flake-utils.lib.eachDefaultSystem (
-      system: let
+      system:
+      let
         pkgs = nixpkgs.legacyPackages.${system};
-        prettier = pkgs.callPackage ./prettier.nix {};
-      in {
+        prettier = pkgs.callPackage ./prettier.nix { };
+        npmlock2nix' = pkgs.callPackage npmlock2nix { };
+
+        gatsbydev = npmlock2nix'.v2.shell {
+          src = ./gatsby;
+        };
+
+        gatsby-node-modules = npmlock2nix'.v2.node_modules {
+          src = ./gatsby;
+        };
+
+        gatsby-build = npmlock2nix'.v2.build {
+          src = ./gatsby;
+          installPhase = "cp -r dist $out";
+          buildCommands = [ "npm run build" ];
+        };
+      in
+      {
         devShells.default = pkgs.mkShell {
           packages = [
             pkgs.alejandra
             pkgs.hugo
             prettier
+            #pkgs.nodePackages.gatsby-cli
+            gatsbydev
           ];
+        };
+
+        packages = {
+          inherit gatsbydev gatsby-node-modules gatsby-build;
         };
       }
     );
