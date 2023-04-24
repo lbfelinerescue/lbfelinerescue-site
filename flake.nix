7176c8583b34bc54a9554562250ea951672766cb
Dan Baker <daniel.n.baker@gmail.com>
2023-04-24 12:19:16 -0700

    Update gatsby packages (#4)
    * added new folder gatsby
    
    * working state
    
    * finally got gatsby working in nix.
    
    * added gatsby to the devshell
    
    * remove image that shouldn't be there.
    
    ---------
    
    Co-authored-by: sadiq <mail@sadiq-pasha.com>

--- flake.nix
+++ flake.nix
@@ -4,24 +4,55 @@
   inputs.flake-compat.url = "github:edolstra/flake-compat";
   inputs.flake-compat.flake = false;
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.npmlock2nix.url = "github:nix-community/npmlock2nix";
+  inputs.npmlock2nix.flake = false;
+  inputs.nix-npm-buildpackage.url = "github:serokell/nix-npm-buildpackage";
+  inputs.nix-npm-buildpackage.flake = false;
 
   outputs = {
     self,
     nixpkgs,
     flake-compat,
     flake-utils,
+    npmlock2nix,
+    nix-npm-buildpackage,
   } @ inputs:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
         prettier = pkgs.callPackage ./prettier.nix {};
+        npmlock2nix' = pkgs.callPackage npmlock2nix {};
+
+        gatsby-node-modules = npmlock2nix'.v2.node_modules {
+          src = ./gatsby;
+          nodejs = pkgs.nodejs;
+        };
+
+        gatsby = pkgs.writeShellApplication {
+          name = "gatsby";
+          text = ''
+            ${gatsby-node-modules}/bin/gatsby "$@"
+          '';
+        };
       in {
         devShells.default = pkgs.mkShell {
           packages = [
             pkgs.alejandra
             pkgs.hugo
             prettier
+            gatsby
           ];
+        };
+
+        devShells.npm = pkgs.mkShell {
+          packages = [
+            pkgs.nodePackages.npm
+            pkgs.nodejs
+          ];
+        };
+
+        packages = {
+          inherit gatsby-node-modules;
         };
       }
     );
